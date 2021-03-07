0023904ae6a6d602dd7d3f0880ffe0c416a63073
nadiia <nadiia.oleks@gmail.com>
2021-03-07 15:21:01 +0200

    set up the Player component to receive props and pass props down to the Counter component

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const Header = () => (
+const Header = (props) => (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     )
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
-            <span className="player-name">Guil</span>
-            <Counter />
+            <span className="player-name">{props.name}</span>
+            <Counter score={props.score}/>
         </div>
     );
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
-                <span className="counter-score">35</span>
+                <span className="counter-score">{props.score}</span>
                 <button className="counter-action increment"> + </button>
             </div>
     )
@@ -28,10 +28,13 @@ const Counter = () => {
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header/>
+            <Header title="Scoreboard" totalPlayers = {1} />
 
             {/*Players list */}
-            <Player/>
+            <Player name="Guil" score={50}/>
+            <Player name="Mike" score={64}/>
+            <Player name="Tom" score={43}/>
+            <Player name="Brad" score={45}/>
         </div>
     );
 }
